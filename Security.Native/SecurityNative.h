3e90f43e46f8cb1a565d7ce45d0c4832ecc7186c
Jim Van Steenwegen <vansteenwegenjim@gmail.com>
2026-08-03 12:18:54 +0200

    export for native dll

--- Security.Native/SecurityNative.h
+++ Security.Native/SecurityNative.h
@@ -2,5 +2,9 @@
 
 extern "C"
 {
-	__declspec(dllexport) bool PingDriver();
+	__declspec(dllexport) bool Ping();
+	__declspec(dllexport) bool Connect();
+	__declspec(dllexport) bool Authenticate();
+	__declspec(dllexport) bool SendPrivateKeyToDriver(const char* privateKeyPath, char* mode);
+	__declspec(dllexport) bool SendPrivateKeyToDriver(char* privateKey);
 }
